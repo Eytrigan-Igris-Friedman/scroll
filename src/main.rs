a1396a65f1984b706a082a4f40d16b6161e1a652
Eytrigan-Igris-Friedman <cbv1307@proton.me>
2025-05-21 23:14:01 +0300

    First step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    match args.len() {
+        1 => println!("No argment passed"),
+        2 => println!("File: {}", args[1]),
+        _ => (),
+    }
 }
